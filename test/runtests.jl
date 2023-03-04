0a73db20b9bd3dd74108709ba11f5427cb540ac1
Giorgio Calderone <giorgio.calderone@gmail.com>
2023-03-04 13:21:39 +0100

    Updated

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,7 +19,7 @@ best, res = fit(model, data)
 
 
 GFitViewer.serialize_json("gfitviewer_test01.json", best)
-GFitViewer.serialize_json("gfitviewer_test02.json", best, res, data)
+GFitViewer.serialize_json("gfitviewer_test02.json", best, res)
 GFitViewer.serialize_json("gfitviewer_test03.json", rebin=2, best)
 GFitViewer.serialize_json("gfitviewer_test04.json", rebin=2, best, res, data,
                           title="Test", xlab="Abscissa", ylab="Ordinate", xr=[0.5, 4.5], yr=[0, 3],
@@ -45,5 +45,5 @@ best, res = fit(multi, data)
 meta1 = GFitViewer.Meta(title="First" , xlab="Abscissa", ylab="Ordinate", xr=[-4, 4], rebin=2, xscale=1000, yscale=1e-17, xunit="Angstrom", yunit="erg s^-1 cm^-2 A^-1")
 meta2 = GFitViewer.Meta(title="Second", xlab="Abscissa", ylab="Ordinate", xr=[-4, 4], rebin=2, xscale=1000, yscale=1e-17, xunit="Angstrom", yunit="erg s^-1 cm^-2 A^-1")
 GFitViewer.serialize_json("gfitviewer_test5.json", multi, xlab="Abscissa", ylab="Ordinate", xr=[-4, 4])
-GFitViewer.serialize_json("gfitviewer_test6.json", best, [meta1, meta2])
+GFitViewer.serialize_json("gfitviewer_test6.json", best, res, [meta1, meta2])
 GFitViewer.serialize_json("gfitviewer_test7.json", best, res, data, [meta1, meta2])
